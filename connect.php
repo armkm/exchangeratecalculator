ddca05024bf3e66fe1b3feda64456ca31cacc400
armkritt <armdonyang@gmail.com>
2018-02-27 04:19:22 +0700

    connectfile upload

--- connect.php
+++ connect.php
@@ -1,15 +1,15 @@
 <?php
 //    if($_SERVER['HTTP_HOST']=='localhost'){
-        $DB_SERVER = 'localhost';
-        $DB_USER_READER = 'root';
-        $DB_PASS_READER = 'Silpakorn60';
-        $DB_NAME = 'db13580183';
+//        $DB_SERVER = 'localhost';
+//        $DB_USER_READER = 'root';
+//        $DB_PASS_READER = 'Silpakorn60';
+//        $DB_NAME = 'db13580183';
 //    }
 //    else{
-//        $DB_SERVER = 'localhost';
-//        $DB_USER_READER = 'u13580183';
-//        $DB_PASS_READER = 'Z#K}Va9xwe';
-//        $DB_NAME = 'db13580183';
+        $DB_SERVER = 'localhost';
+        $DB_USER_READER = 'u13580183';
+        $DB_PASS_READER = 'Z#K}Va9xwe';
+        $DB_NAME = 'db13580183';
 //    }
 
     $conn = new mysqli($DB_SERVER,$DB_USER_READER,$DB_PASS_READER,$DB_NAME);
